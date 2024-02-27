e25ee95cb9582ec630a69fb26fec5b86da4b562b
susumu <2j46v723kn4@gmail.com>
2024-02-27 23:04:17 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test/{room}/{id}', [TestController::class, 'index']);
+Route::get('/hello',[HelloController::class, 'index']);
+Route::get('/',function(){
+  return view('hello');
 });
